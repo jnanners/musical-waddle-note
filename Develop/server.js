a51a3433dbb4c7bf7b4db4fa229b2f7e86a1f19c
JBanana <johnandrewharris3@gmail.com>
2021-10-24 13:57:49 -0500

    change for heroku

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,8 @@
+const PORT = process.env.PORT || 3001;
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
